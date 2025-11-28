d4ff13cf915491dd2cef2672ba8e753333fb3d75
AaySquare <aayushmathur14@gmail.com>
2025-11-28 00:45:15 +0000

    Created Game class and moved the logic of displaying the window there instead.

---
+++ src/Game.cpp
@@ -0,0 +1,44 @@
+﻿#include "Game.h"
+
+Game::Game()
+{
+    window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "Asteroids");
+    window.setFramerateLimit(144);
+
+    // Create Player Ship
+    // Create initial asteroid
+}
+
+void Game::Run()
+{
+    while (window.isOpen())
+    {
+        const float deltaTime = clock.restart().asSeconds();
+
+        ProcessInput();
+        Update(deltaTime);
+        Render();
+    }
+}
+
+void Game::ProcessInput()
+{
+    while (const std::optional event = window.pollEvent())
+    {
+        if (event->is<sf::Event::Closed>())
+        {
+            window.close();
+        }
+    }
+}
+
+void Game::Update(float deltaTime)
+{
+
+}
+
+void Game::Render()
+{
+    window.clear();
+    window.display();
+}
---
+++ src/Game.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include "SFML/Graphics/RenderWindow.hpp"
+
+class Game
+{
+public:
+    Game();
+    void Run();
+
+private:
+    void ProcessInput();
+    void Update(float deltaTime);
+    void Render();
+
+private:
+    sf::RenderWindow window;
+    sf::Clock clock;
+};
